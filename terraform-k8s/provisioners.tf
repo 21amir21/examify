541cde8fc7aba428e1cf9ed247084866d459beda
Amir <amirosama21@gmail.com>
2025-03-31 18:41:17 +0200

    feat: enhance local provisioners for Kubernetes cluster configuration and metrics server deployment

--- terraform-k8s/provisioners.tf
+++ terraform-k8s/provisioners.tf
@@ -1,5 +1,13 @@
 resource "null_resource" "local_provisioner_doctl" {
   provisioner "local-exec" {
-    command = "doctl kubernetes cluster kubeconfig save "
+    command = "doctl kubernetes cluster kubeconfig save ${digitalocean_kubernetes_cluster.examify-k8s-cluster.id}"
   }
+  depends_on = [kubernetes_service.examify-infra]
+}
+
+resource "null_resource" "local_provisioner_kubectl" {
+  provisioner "local-exec" {
+    command = "kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml"
+  }
+  depends_on = [null_resource.local_provisioner_doctl]
 }
