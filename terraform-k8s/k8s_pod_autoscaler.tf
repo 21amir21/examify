77ec03a3db050efcf58b112ffa3646de869c511e
Amir <amirosama21@gmail.com>
2025-03-31 18:41:50 +0200

    feat: add Kubernetes horizontal pod autoscaler for examify-infra deployment

---
+++ terraform-k8s/k8s_pod_autoscaler.tf
@@ -0,0 +1,27 @@
+resource "kubernetes_horizontal_pod_autoscaler" "autoscaler" {
+  metadata {
+    name = "examify-infra-autoscaler"
+  }
+  spec {
+    min_replicas = 1
+    max_replicas = 10
+
+    scale_target_ref {
+      api_version = "apps/v1"
+      kind        = "Deployment"
+      name        = kubernetes_deployment.examify-infra.metadata.0.name
+    }
+
+    metric {
+      type = "Resource"
+      resource {
+        name = "cpu"
+        target {
+          type                = "Utilization"
+          average_utilization = 80
+        }
+      }
+    }
+  }
+  depends_on = [null_resource.local_provisioner_kubectl]
+}
