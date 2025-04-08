45e72850b59236f3f8d58374b2a0f03e116cb655
Amir <amirosama21@gmail.com>
2025-04-08 06:49:59 +0200

    fix: update JSON tags for Password field in Instructor and Student models, as they will now be hashed with bcrypt, so no need to omit them

--- system/backend/models/student.go
+++ system/backend/models/student.go
@@ -6,7 +6,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Student struct {
 	ID              primitive.ObjectID   `bson:"_id,omitempty" json:"id"`
 	Username        string               `bson:"username" json:"username"`
-	Password        string               `bson:"password,omitempty" json:"-"` // Exclude password from JSON responses or hash it
+	Password        string               `bson:"password,omitempty" json:"password"` // Exclude password from JSON responses or hash it
 	Name            string               `bson:"name" json:"name"`
 	EnrolledCourses []primitive.ObjectID `bson:"enrolledCourses" json:"enrolledCourses"`
 	EnrolledExams   []primitive.ObjectID `bson:"enrolledExams" json:"enrolledExams"`
