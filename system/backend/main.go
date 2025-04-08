50a3266c54ed77e2dd9ffb273c01f081eaf1799f
Amir <amirosama21@gmail.com>
2025-04-08 05:17:31 +0200

    feat: update main.go to initialize Gin router

--- system/backend/main.go
+++ system/backend/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Hello World!")
+
+	r := gin.Default()
+
+	r.Run(":8080")
+
+	log.Println("Hello from logger")
 }
