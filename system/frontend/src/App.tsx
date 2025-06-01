f6591e6c879dde2a9a6b0aad5e1f36038b44129f
Amir <amirosama21@gmail.com>
2025-06-01 19:09:17 +0300

    feat: add routing for student and instructor pages in App component (hopefully finishing frontend)

--- system/frontend/src/App.tsx
+++ system/frontend/src/App.tsx
@@ -3,8 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AuthProvider from "./store/auth/AuthProvider";
 import CurrentExamProvider from "./store/exam/CurrentExamProvider";
 import Layout from "./components/Layout";
-// import MainRoutes from "./MainRoutes"; // Assuming you're using React Router
 import LoginPage from "./pages/LoginPage";
+import StudentsDashboard from "./pages/students/StudentsDashboard";
+import InstructorsDashboard from "./pages/instructors/InstructorsDashboard";
+import InstructorExamsPage from "./pages/instructors/exams/InstructorExamsPage";
+import CreateExamPage from "./pages/instructors/exams/CreateExamPage";
+import StudentExamsPage from "./pages/students/exams/StudentExamsPage";
+import CurrentExamPage from "./pages/students/exams/CurrentExamPage";
+import InvigilationPage from "./pages/instructors/exams/InvigilationPage";
 
 const App: React.FC = () => {
   return (
@@ -14,6 +20,25 @@ const App: React.FC = () => {
           <Layout>
             <Routes>
               <Route path="/login" element={<LoginPage />} />
+              <Route path="/students" element={<StudentsDashboard />} />
+              <Route path="/students/exams" element={<StudentExamsPage />} />
+              <Route
+                path="/students/exams/current-exam"
+                element={<CurrentExamPage />}
+              />
+              <Route path="/instructors" element={<InstructorsDashboard />} />
+              <Route
+                path="/instructors/exams"
+                element={<InstructorExamsPage />}
+              />
+              <Route
+                path="/instructors/exams/create-exam"
+                element={<CreateExamPage />}
+              />
+              <Route
+                path="/instructors/exams/invigilation"
+                element={<InvigilationPage />}
+              />
             </Routes>
           </Layout>
         </CurrentExamProvider>
