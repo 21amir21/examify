74e846eaa7065ba630c0f14d06c12160c10237c2
Amir <amirosama21@gmail.com>
2025-06-03 23:10:54 +0300

    fix: update docker-compose and entrypoint scripts for improved volume handling and build process

--- system/backend/docker-entrypoint.sh
+++ system/backend/docker-entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 terraform -chdir=./terraform/exam_instance init
 terraform -chdir=./terraform/exam_vpc init
-./main  # Run your Go binary
+
+echo "Building Go binary inside container..."
+go build -o main main.go
+
+./main # Run your Go binary
