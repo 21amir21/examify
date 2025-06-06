3232e4fb56427ef3781c45b300377c9281ae7dac
Amir <amirosama21@gmail.com>
2025-06-06 04:12:34 +0300

    feat: add TypeScript declarations for CSS and SCSS modules

--- system/frontend/src/global.d.ts
+++ system/frontend/src/global.d.ts
@@ -1,1 +1,12 @@
 declare module "*.css";
+
+declare module "*.module.css" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+// for feature use of SCSS
+declare module "*.module.scss" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
